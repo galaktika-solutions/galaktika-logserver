91d8dfd5ee390c3455682d2325908edc630f7679
gegyed <gabor.egyed@vertis.com>
2017-07-25 08:12:16 +0200

    done

--- docker/backup/backup.sh
+++ docker/backup/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 DATE=$(date +%Y-%m-%d-%H-%M)
-rsync -rvu --ignore-existing --recursive --delete /usr/share/elasticsearch/data $BACKUP_DEST
+rsync -rvu --ignore-missing-args --ignore-existing --recursive --delete /usr/share/elasticsearch/data $BACKUP_DEST
 echo $DATE > /home/myvertis/last_file_backup
 rsync -rvu /home/myvertis/last_file_backup $BACKUP_DEST
