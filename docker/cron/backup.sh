c214d63269883d9b97ffb45f6913c7e5358182e6
gegyed <gabor.egyed@vertis.com>
2017-07-27 15:29:59 +0200

    done

--- docker/cron/backup.sh
+++ docker/cron/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 DATE=$(date +%Y-%m-%d-%H-%M)
-rsync -rvu --ignore-existing --recursive --delete /mount/backups/my_backup $BACKUP_DEST
+rsync -rv --delete /mount/backups/my_backup $BACKUP_DEST
 echo $DATE > /home/myvertis/last_file_backup
-rsync -rvu /home/myvertis/last_file_backup $BACKUP_DEST
+rsync -rv /home/myvertis/last_file_backup $BACKUP_DEST
