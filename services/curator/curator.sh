f93a99e37515697fa661e12146229fb8a8e833ea
gegyed <gabor.egyed@vertis.com>
2018-11-06 15:58:59 +0100

    change last backup method

--- services/curator/curator.sh
+++ services/curator/curator.sh
@@ -7,5 +7,6 @@ set -e
 curator --config /conf/curator.yml /conf/action.yml
 
 # Do the backup
-find /mount/backups/my_backup -maxdepth 1 -regex '/mount/backups/my_backup/last-backup-.*' -delete
-touch "/mount/backups/my_backup/last-backup-$(date -u +"%Y-%m-%d-%H-%M-%Z")"
+mkdir /mount/backups/my_backup/last_backup/
+find /mount/backups/my_backup -maxdepth 1 -regex '/mount/backups/my_backup/last_backup/last-backup-.*' -delete
+touch "/mount/backups/my_backup/last_backup/last-backup-$(date -u +"%Y-%m-%d-%H-%M-%Z")"
