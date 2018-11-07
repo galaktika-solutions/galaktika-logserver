4023c6d0ecf92a544375d57100c7453fccda6470
gegyed <gabor.egyed@vertis.com>
2018-11-07 13:06:18 +0100

    backup user

--- services/curator/curator.sh
+++ services/curator/curator.sh
@@ -8,5 +8,6 @@ curator --config /conf/curator.yml /conf/action.yml
 
 # Do the backup
 mkdir -p /mount/backups/my_backup/last_backup/
+chown -R "$BACKUP_UID:$BACKUP_UID" /mount/backups/my_backup/
 find /mount/backups/my_backup -maxdepth 1 -regex '/mount/backups/my_backup/last_backup/last-backup-.*' -delete
 touch "/mount/backups/my_backup/last_backup/last-backup-$(date -u +"%Y-%m-%d-%H-%M-%Z")"
